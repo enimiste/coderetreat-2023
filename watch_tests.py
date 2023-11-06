e45df425104ec1f53d6a737b6a1dbfde966aee4f
Enimiste <nouni.elbachir@gmail.com>
2023-11-06 00:20:58 +0000

    add file watcher to run tests continuously

---
+++ watch_tests.py
@@ -0,0 +1,33 @@
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import os
+import re
+
+class  MyHandler(FileSystemEventHandler):
+    def  on_modified(self,  event):
+        print(f'event type: {event.event_type} path : {event.src_path}')
+        if re.match('.*/test_(.*)\.py', event.src_path):
+            os.system('clear')
+            os.system('pytest')
+        print('Type <Ctrl+C> to stop the watcher')
+        print("Watching for test files changes ...")
+
+    def  on_created(self,  event):
+        pass
+    def  on_deleted(self,  event):
+        pass
+
+if __name__=="__main__":
+    print("Start watcher")
+    event_handler = MyHandler()
+    observer = Observer()
+    observer.schedule(event_handler,  path='.',  recursive=False)
+    observer.start()
+
+    try:
+        while  True:
+            time.sleep(1)
+    except  KeyboardInterrupt:
+        observer.stop()
+        observer.join()
