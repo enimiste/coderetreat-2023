49cb158783ef5faab05116b83d729a0a00ae0e20
Enimiste <nouni.elbachir@gmail.com>
2023-11-06 15:14:45 +0000

    fix bug : double run

--- watch_tests.py
+++ watch_tests.py
@@ -4,14 +4,30 @@ from watchdog.events import FileSystemEventHandler
 import os
 import re
 
+"""
+Requirements :
+- python 3.9
+- pytest==7.4.3
+- pytest-cov==4.1.0
+- watchdog==3.0.0
+"""
+
 class  MyHandler(FileSystemEventHandler):
+    def __init__(self) -> None:
+        self.has_changes = False
+
     def  on_modified(self,  event):
         # print(f'event type: {event.event_type} path : {event.src_path}')
         if re.match('.*/test_(.*)\.py', event.src_path):
+            self.has_changes = True
+
+    def run_tests(self):
+        if self.has_changes:
             os.system('clear')
             os.system('pytest --cov')
             print('Type <Ctrl+C> to stop the watcher')
             print("Watching for test files changes ...")
+            self.has_changes = False
 
     def  on_created(self,  event):
         pass
@@ -30,6 +46,7 @@ if __name__=="__main__":
 
     try:
         while  True:
+            event_handler.run_tests()
             time.sleep(1)
     except  KeyboardInterrupt:
         observer.stop()
