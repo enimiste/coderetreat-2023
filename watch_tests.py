489ed1eaf21ebf304434aa3400434e3b25c17980
Enimiste <nouni.elbachir@gmail.com>
2023-11-06 00:26:15 +0000

    edit watcher

--- watch_tests.py
+++ watch_tests.py
@@ -6,20 +6,23 @@ import re
 
 class  MyHandler(FileSystemEventHandler):
     def  on_modified(self,  event):
-        print(f'event type: {event.event_type} path : {event.src_path}')
+        # print(f'event type: {event.event_type} path : {event.src_path}')
         if re.match('.*/test_(.*)\.py', event.src_path):
             os.system('clear')
             os.system('pytest')
-        print('Type <Ctrl+C> to stop the watcher')
-        print("Watching for test files changes ...")
+            print('Type <Ctrl+C> to stop the watcher')
+            print("Watching for test files changes ...")
 
     def  on_created(self,  event):
         pass
+    
     def  on_deleted(self,  event):
         pass
 
 if __name__=="__main__":
-    print("Start watcher")
+    os.system('clear')
+    print("Watching for test files changes ...")
+    print('Type <Ctrl+C> to stop the watcher')
     event_handler = MyHandler()
     observer = Observer()
     observer.schedule(event_handler,  path='.',  recursive=False)
